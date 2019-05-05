ca643a612bb8f7e41b587746bee50af0edf563a3
Tyoff45 <bjur.parker45@gmail.com>
2019-05-05 10:08:52 -0700

    jaws db working

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,14 @@ require('dotenv').config()
 // Set the port of our application
 
 // MySQL DB Connection Information (remember to change this with our specific credentials)
-if (process.env.CLEARDB_DATABASE_URL) {
+if (process.env.JAWS_DB_URL) {
     console.log ("------")
     console.log (process.env)
-    let connection = mysql.createConnection("mysql://b5dyae5tprc2m3cy:xu0w01o205cfu5bq@alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yejc48k8euy0jecs");
+    connection = mysql.createConnection("mysql://b5dyae5tprc2m3cy:xu0w01o205cfu5bq@alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yejc48k8euy0jecs");
    // mysql://b2c8985c6e27a5:ec1799e7@us-cdbr-iron-east-02.cleardb.net/heroku_d807133fc14176e?reconnect=true
 } else {
-    let connection = mysql.createConnection({
+    console.log("local instance")
+    connection = mysql.createConnection({
         host: 'localhost',
         port: 3306,
         user: "webuser",
